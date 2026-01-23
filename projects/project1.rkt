c82da49d062716b30a94f4832df70469694a487a
bozrem <bozungremy@gmail.com>
2026-01-22 23:25:21 -0600

    Part 4 (Advanced Forest)

--- projects/project1.rkt
+++ projects/project1.rkt
@@ -62,8 +62,11 @@
 (define (tree)
   (define trunk (rectangle 50 150 "solid" "brown"))
   (define leaves (isosceles-triangle 200 55 "solid" "medium forest green")) ;; Found the fancy list https://docs.racket-lang.org/draw/color-database___.html
+  (define leaves-outline (isosceles-triangle 200 55 "outline" "dark green"))
 
-  (overlay/offset leaves 0 50 trunk)
+  (define full-leaves (overlay leaves-outline leaves))
+
+  (overlay/offset full-leaves 0 50 trunk)
   )
 
 ; 2.4: Car
@@ -156,16 +159,76 @@
 ;;; Part 4: Recursion with Graphics
 
 ; 4.1: Row of Images
-
+(define (row-of num img)
+  (cond
+    [(= 1 num)  img]
+    [else       (above img (row-of (- num 1) img))]
+    )
+  )
 
 ; 4.2: Column of Images
-
+(define (column-of num img)
+  (cond
+    [(= 1 num)  img]
+    [else       (beside img (column-of (- num 1) img))]
+    )
+  )
 
 ; 4.3: Grid of Images
+(define (grid-of rows cols img)
+  (define row (row-of rows img))
 
+  (column-of cols row)
+  )
 
 ; 4.4: Forest
 
+;; I want to do sort of scattered rows that make it actually look like you're looking into a forest
+;; I'm thinking:
+;; 
+;;  We start at some small value
+;;  That value is the distance scale
+;;  Need a function that translates a distance to a scale and y offset
+;;  Get a random value for the X position
+;;  Start from the back so that they get layered correctly
+;;  
+;;
+(define (forest num-trees)
+  (define Y_OFFSET_PER 2.5) ;; The additional Y offset added each tree
+  (define TREE (tree)) ;; Just to avoid calling it over and over
+  (define IMAGE_HEIGHT (+ (image-height TREE) (* Y_OFFSET_PER num-trees)))
+  (define IMAGE_WIDTH 500)
+
+  (define START_SCALE 0.2)
+
+  (define BACKGROUND (rectangle IMAGE_WIDTH IMAGE_HEIGHT 255 "white")) ;; 255 makes it transparent background
+
+  (define (random-x) (* (random) IMAGE_WIDTH))
+  (define (scale->y scl)
+    (- (/ IMAGE_HEIGHT 2)
+      (* Y_OFFSET_PER 
+         (/ (- 1 scl) SCALE_PER_TREE)
+        )
+      )
+    )
+  ;; _ is the amount of trees I've gone up
+  ;; Smaller scales should make a higher Y
+  ;; I have the current scale
+  ;; The scale traveled so far is 1 - current scale
+  ;; If we devide that by how many scales we have per tree, we get how many trees up we are
+  ;; We can multiply that to get the Y
+
+  (define SCALE_PER_TREE (/ (- 1 START_SCALE) num-trees))
+
+  (define (add-trees scl)
+    (cond
+      [(> START_SCALE scl)  BACKGROUND]
+      [else                 (place-image (scale scl TREE) (random-x) (scale->y scl) (add-trees (- scl SCALE_PER_TREE)))]
+      )
+    )
+
+  (add-trees 1.0)
+  )
 
 
 
